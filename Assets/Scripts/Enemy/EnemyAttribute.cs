86e8cd10457e2259d6f27a61c892de1a25aa67d2
Fish <fishyu624@gmail.com>
2018-01-07 00:28:41 +0800

    Save the project

--- Assets/Scripts/Enemy/EnemyAttribute.cs
+++ Assets/Scripts/Enemy/EnemyAttribute.cs
@@ -2,30 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAttribute : MonoBehaviour {
+public class EnemyAttribute : MonoBehaviour
+{
 
     public int startingHealth = 100;
     public int currentHealth;
     public int exp = 10;
     public float destoryDelay = 1.0f;
+    public float attackSpeed = 1.0f;
 
     bool isDead = false;
     Animator anim;
     UnityEngine.AI.NavMeshAgent nav;
-    Transform playerTransform;
+    Transform playerPosition;
     PlayerAttribute pa;
+    float timer;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Vector3.Distance(playerPosition.position, transform.position) < 2) {
+            nav.enabled = false;
+            anim.SetBool("IsMoving", false);
+            if (timer >= attackSpeed)
+            {
+                anim.SetTrigger("Attack");
+                timer = 0;
+            }
+        }
+        else if (Vector3.Distance(playerPosition.position, transform.position) < 6)
+        {
+            nav.enabled = true;
+            anim.SetBool("IsMoving", true);
+            nav.SetDestination(playerPosition.position);
+            transform.LookAt(playerPosition);
+        }
+        else
+        {
+            nav.enabled = false;
+            anim.SetBool("IsMoving", false);
+        }
+    }
 
     public void TakeDamage(int amount)
     {
@@ -35,7 +62,7 @@ public class EnemyAttribute : MonoBehaviour {
         currentHealth -= amount;
         anim.SetTrigger("Damage");
 
-        if (currentHealth <= 0) 
+        if (currentHealth <= 0)
         {
             GameObject player = GameObject.FindGameObjectWithTag("Player");
             player.GetComponent<PlayerAttribute>().GainExp(exp);
