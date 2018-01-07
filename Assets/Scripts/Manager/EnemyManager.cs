ffa216a2e92d217a7001c473076d604409be29d2
Fish <fishyu624@gmail.com>
2018-01-07 21:44:40 +0800

    Add enemy spawn script

--- Assets/Scripts/Manager/EnemyManager.cs
+++ Assets/Scripts/Manager/EnemyManager.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
 
+    public Transform[] SpawnPoint;
+    public GameObject enemy;
+
+    Dictionary<Vector3, GameObject> enemyExistMap;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        foreach(Transform t in SpawnPoint) {
+            Spawn(t);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        foreach(KeyValuePair<Vector3, GameObject> temp in enemyExistMap) {
+            print(temp.Value);
+        }
 	}
+
+    void Spawn(Transform t) {
+        GameObject SpawnedEnemy = Instantiate(enemy, t.position, t.rotation);
+        enemyExistMap.Add(t.position, SpawnedEnemy);
+    }
 }
