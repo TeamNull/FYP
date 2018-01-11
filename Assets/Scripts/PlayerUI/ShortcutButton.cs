22b90abb7c7ec14b4912c8a26c521af35b25a2a9
cckhhu6495 <cckhhu6495@gmail.com>
2018-01-11 17:20:09 +0800

    add one btn

---
+++ Assets/Scripts/PlayerUI/ShortcutButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortcutButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("ShortcutBtn0");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ShortcutBtn0 () {
+        Debug.Log("ShortcutBtn0");
+    }
+}
