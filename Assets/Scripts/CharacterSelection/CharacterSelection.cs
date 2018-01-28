0024aa05e6ec3367b545b503b36bb8be56d52f27
Harry <harry_hong14@hotmail.com>
2018-01-28 19:55:06 +0800

    Created character creation scene with character selection control and scene changing

---
+++ Assets/Scripts/CharacterSelection/CharacterSelection.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour {
+
+    private GameObject[] characterObjectList;
+    private int index = 0;
+
+    private void Start()
+    {
+        index = PlayerPrefs.GetInt("CharacterSelected");
+
+        characterObjectList = new GameObject[transform.childCount];
+
+        // Fill the characterobjectlist array with models
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            characterObjectList[i] = transform.GetChild(i).gameObject;      
+        }
+
+        // Toogle off renderer for all elements in the list
+        foreach (GameObject go in characterObjectList)
+        {
+            go.SetActive(false);
+        }
+
+        // Toogle on current character
+        if (characterObjectList[index])
+        {
+            characterObjectList[index].SetActive(true);
+        }
+    }
+
+    public void ToggleLeft()
+    {
+        // Toogle off the current model
+        characterObjectList[index].SetActive(false);
+
+        index = index - 1;
+        if (index < 0)
+        {
+            index = characterObjectList.Length - 1;
+        }
+
+        // Toogle on the current model
+        characterObjectList[index].SetActive(true);
+    }
+
+    public void ToggleRight()
+    {
+        // Toogle off the current model
+        characterObjectList[index].SetActive(false);
+
+        index = index + 1;
+        if (index == characterObjectList.Length)
+        {
+            index = 0;
+        }
+
+        // Toogle on the current model
+        characterObjectList[index].SetActive(true);
+    }
+
+    public void ConfirmSelection()
+    {
+        PlayerPrefs.SetInt("CharacterSelected", index);
+        SceneManager.LoadScene("VillageTesting");
+    }
+}
