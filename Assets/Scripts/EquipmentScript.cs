f80af7a835c50c094d5d2d1f6c4c173125bb916f
cckhhu6495 <cckhhu6495@gmail.com>
2018-02-11 02:52:57 +0800

    add equipment detail

---
+++ Assets/Scripts/EquipmentScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentScript : MonoBehaviour {
+    public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+        panel.SetActive(!panel.activeSelf);
+
+    }
+
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on GameObject.");
+        panel.SetActive(!panel.activeSelf);
+    }
+}
