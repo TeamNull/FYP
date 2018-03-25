4e6e31cd8c5b1dad8c188eb6945f4d29217ce154
Fish <fishyu624@gmail.com>
2018-03-25 23:14:37 +0800

    Fix bag error

--- Assets/Inventory/Scripts/MonoBehaviours/Inventory/Bag.cs
+++ Assets/Inventory/Scripts/MonoBehaviours/Inventory/Bag.cs
@@ -24,6 +24,7 @@ public class Bag : MonoBehaviour
             itemUIList.Add(transform.GetChild(i));
         }
         itemUIList.OrderBy(x => x.name);
+        this.gameObject.transform.parent.gameObject.SetActive(false);
     }
 
     public void AddItem(Item item, int unit)
