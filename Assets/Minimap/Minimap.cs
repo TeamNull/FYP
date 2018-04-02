d16ec75eaf941f69213378fa263878c641d3285d
Fish <fishyu624@gmail.com>
2018-04-02 20:36:10 +0800

    Fix minimap bug

--- Assets/Minimap/Minimap.cs
+++ Assets/Minimap/Minimap.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Minimap : MonoBehaviour {
 
+    float smooth = 10f;
     Transform standardPos;          // Camera normal positionLIHKG
 
 	// Use this for initialization
 	void Start () {
+        
+	}
+
+    public void Init() {
         standardPos = GameObject.Find("MinimapCamPos").transform;
 
         transform.position = standardPos.position;
         transform.forward = standardPos.forward;
         transform.rotation = standardPos.rotation;
-	}
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        transform.position = standardPos.position;
-        transform.forward = standardPos.forward;
+    void FixedUpdate () {
+        if (standardPos != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, standardPos.position, Time.fixedDeltaTime * smooth);
+            transform.forward = Vector3.Lerp(transform.forward, standardPos.forward, Time.fixedDeltaTime * smooth);
+        }
 	}
 }
