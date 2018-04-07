2615a45e00efc086293853e90d78936b74d14e79
cckhhu6495 <cckhhu6495@gmail.com>
2018-04-07 15:32:28 +0800

    finish the monster adding in ruins change the spider to jT in forest

--- Assets/Scripts/Manager/LoadSceneManager.cs
+++ Assets/Scripts/Manager/LoadSceneManager.cs
@@ -113,6 +113,7 @@ public class LoadSceneManager : MonoBehaviour
         if (sceneName == "Ruins")
         {
             SceneManager.GetSceneByName(sceneName).GetRootGameObjects()[0].GetComponent<EnemyManager>().Init();
+            //SceneManager.GetSceneByName(sceneName).GetRootGameObjects()[1].GetComponent<EnemyManager>().Init();
             //SceneManager.GetSceneByName(sceneName).GetRootGameObjects()[0].GetComponent<Weather>().SwitchSkyBox(3);
         }
         playerTransform.position = v3;
