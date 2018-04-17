7b7d34b872d7d38af7051baf1e3641e360ca1787
Fish <fishyu624@gmail.com>
2018-04-17 18:05:19 +0800

    Add Skill item

---
+++ Assets/Inventory/Scripts/ScriptableObjects/Inventory/Skill.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Skill : Item {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void ApplyAction()
+    {
+        
+    }
+}
